b13ad73f0cf98daa3ca30b30ff7bbd9865982942
Chaz Leong <cleong14@gmail.com>
2017-01-28 18:51:01 -1000

    api key commented in for testing purposes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function() {
+  console.log('Listening to port', server.address().port);
+});
+
+// sWmsVOKo5dvkgiEuL07K2bxhYCgCfkffvrrAWdIU
